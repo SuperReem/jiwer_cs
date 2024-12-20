df711405f7535421bb22a5d3ae99dd6bdfeabfca
Reem <reem66am@gmail.com>
2024-12-20 19:17:03 +0300

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,4 +1,5 @@
 import re
+from tabulate import tabulate
 
 def is_arabic(text):
     """
@@ -11,3 +12,20 @@ def is_arabic(text):
         bool: True if the text is classified as Arabic, False otherwise.
     """
     return re.search(r'[\u0600-\u06FF\u0750-\u077F\u08A0-\u08FF]+', text)
+
+
+
+def display_metrics_table(metrics, title, headers=None):
+    """
+    Prints the given metrics in a formatted table.
+
+    Args:
+    metrics (list of tuples): List of tuples containing metric names and their values.
+    headers (list of str, optional): List of column headers. If None, default headers will be used.
+    """
+    if headers is None:
+        headers = ["Metric", "Value"]  # Default headers
+        
+    print(f"### {title} ###")
+    print(tabulate(metrics, headers=headers, tablefmt="pretty"))
+    print()
